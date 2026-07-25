411a212c2df5e07b982e3ae6a5095d7593a5b0b8
Nafisat SANNI <nafiyoza@gmail.com>
2026-07-25 03:07:20 +0100

    Fix is_returnable checkbox: default to false when unchecked

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -53,7 +53,7 @@ class ProductRequest extends FormRequest
     {
         $this->merge([
             'is_active' => $this->boolean('is_active', true),
-            'is_returnable' => $this->boolean('is_returnable', true),
+            'is_returnable' => $this->boolean('is_returnable', false),
             'discount'  => $this->input('discount', 0),
             'selling_price' => $this->filled('selling_price') ? $this->input('selling_price') : 0,
             'status' => $this->input('status', $this->boolean('is_active', true) ? 'active' : 'inactive'),
